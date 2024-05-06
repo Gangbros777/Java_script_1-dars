ccebd9c17640732d28519767c83fb064930ce464
Gangbros777 <161280210+Gangbros777@users.noreply.github.com>
2024-05-06 09:28:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let userName = prompt("Ismingizni kiriting")
+let userSurname = prompt("Familyangizni kiriting kiriting")
+let userLesson = prompt("Yoqtirgan faningiz")
+let userAddress = prompt("Yashaydigan tumaningizni kiriting")
+let userAge = + prompt("Yoshingizni kiriting")
+let message = alert("Regestratsiya kirganiz uchun raxmat")
+let javob = alert(`Ism:${userName}
+Familiya:${userSurname}
+Yosh:${userAge}
+Tuman:${userAddress}
+Fan:${userLesson}`)
+console.log(`Ism:${userName}
+Familiya:${userSurname}
+Tuman:${userAddress}
+Fan:${userLesson}
+Yosh:${userAge}`);
+String
